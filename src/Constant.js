98df99e5ae5c0ccbe8ac8e384edab58c984b0b3a
xiaoleiooo <sfanglei@126.com>
2019-01-03 23:10:04 +0800

    修改最大键盘高度

--- src/Constant.js
+++ src/Constant.js
@@ -4,7 +4,10 @@ export const MIN_COMPOSER_HEIGHT = Platform.select({
   ios: 33,
   android: 41,
 });
-export const MAX_COMPOSER_HEIGHT = 200;
+export const MAX_COMPOSER_HEIGHT = Platform.select({
+    ios: 92,
+    android: 100,
+});
 export const DEFAULT_PLACEHOLDER = 'Type a message...';
 export const DATE_FORMAT = 'll';
 export const TIME_FORMAT = 'LT';
